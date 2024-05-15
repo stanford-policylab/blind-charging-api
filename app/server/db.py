dbd20deade79c51fe30b102b27960fae81ff9168
Joe Nudell <jnudell@hks.harvard.edu>
2024-05-14 17:45:46 -0700

    schema updates, db schema (#27)

--- app/server/db.py
+++ app/server/db.py
@@ -1,6 +1,90 @@
+from datetime import datetime, timezone
+from typing import List
+
+from sqlalchemy import ForeignKey
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.types import BINARY, DateTime, String
+from typing_extensions import Annotated
+from uuid_utils import UUID, uuid7
+
+str_256 = Annotated[str, 256]
+
+
+def nowts() -> datetime:
+    """Return the current datetime with the timezone set to UTC."""
+    return datetime.now(timezone.utc)
+
+
+def primary_key() -> bytes:
+    """Generate a UUIDv7 primary key."""
+    return uuid7().bytes
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    pass
+    type_annotation_map = {
+        UUID: BINARY(128),
+        datetime: DateTime(timezone=True),
+        str_256: String(256),
+    }
+
+
+class File(Base):
+    __tablename__ = "file"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=primary_key)
+    external_id: Mapped[str_256] = mapped_column(unique=True)
+    jurisdiction_id: Mapped[str_256]
+    case_id: Mapped[str_256]
+    defendants: Mapped[List["DefendantFile"]] = relationship()
+    redactions: Mapped[List["Redaction"]] = relationship(back_populates="file")
+    name: Mapped[str]
+    file_type: Mapped[str_256]
+    file_size: Mapped[int]
+    storage_path: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(default=nowts)
+    updated_at: Mapped[datetime] = mapped_column(default=nowts, onupdate=nowts)
+
+
+class Redaction(Base):
+    __tablename__ = "redaction"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=primary_key)
+    file_id: Mapped[UUID] = mapped_column(ForeignKey("file.id"))
+    file: Mapped["File"] = relationship(back_populates="redactions")
+    storage_path: Mapped[str]
+    status: Mapped[str_256]
+    created_at: Mapped[datetime] = mapped_column(default=nowts)
+    updated_at: Mapped[datetime] = mapped_column(default=nowts, onupdate=nowts)
+
+
+class DefendantFile(Base):
+    __tablename__ = "defendant_file"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=primary_key)
+    file_id: Mapped[UUID] = mapped_column(ForeignKey("file.id"))
+    file: Mapped["File"] = relationship(back_populates="defendants")
+    defendant_id: Mapped[str_256]
+    created_at: Mapped[datetime] = mapped_column(default=nowts)
+    updated_at: Mapped[datetime] = mapped_column(default=nowts, onupdate=nowts)
+
+
+class Events(Base):
+    __tablename__ = "events"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=primary_key)
+    event_type: Mapped[str_256]
+    event_data: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(default=nowts)
+
+
+class ProcessingQueue(Base):
+    __tablename__ = "processing_queue"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=primary_key)
+    file_id: Mapped[UUID] = mapped_column(ForeignKey("file.id"))
+    status: Mapped[str_256] = mapped_column(default="queued")
+    job_id: Mapped[UUID] = mapped_column(nullable=True)
+    callback_url: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(default=nowts)
+    updated_at: Mapped[datetime] = mapped_column(default=nowts, onupdate=nowts)
