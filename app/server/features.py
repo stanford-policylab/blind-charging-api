a54b640bb790e6a4b001c1ba9c55ce812ebcd184
Joe Nudell <jnudell@hks.harvard.edu>
2024-07-16 22:18:18 -0400

    demo experiment

--- app/server/features.py
+++ app/server/features.py
@@ -1,22 +1,34 @@
 from alligater import Alligater, Arm, Feature, Population, PrintLogger, Rollout, Variant
+from crocodsl import parse
+
+from .config import config
 
 gater = Alligater(
-    logger=PrintLogger(),
+    logger=PrintLogger(trace=config.debug),
     features=[
         Feature(
             "ft_blind_review",
             variants=[
                 Variant("blind", True),
                 Variant("control", False),
+                Variant("off", False),
             ],
             rollouts=[
                 Rollout(
-                    name=Rollout.DEFAULT,
-                    population=Population.DEFAULT,
+                    name="demo_experiment",
+                    population=Population.Expression(
+                        parse('$jurisdiction_id Eq "demo"')
+                    ),
                     arms=[
                         Arm("blind", weight=0.5),
                         Arm("control", weight=0.5),
                     ],
+                    sticky=False,
+                ),
+                Rollout(
+                    name=Rollout.DEFAULT,
+                    population=Population.DEFAULT,
+                    arms=["off"],
                     sticky=False,
                 ),
             ],
