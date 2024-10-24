a6a91f6ff8934f84aaad1312d42bde7c45606296
Joe Nudell <jnudell@hks.harvard.edu>
2024-10-24 10:36:02 -0400

    Add more interpretation to the worker healthcheck (#77)
    The existing health check for the worker just returns the `inspect ping`
    result from celery directly. This means that it's up to the caller to
    interpret the results, which is more work than possible on most
    container services. This moves the existing health check to
    `/health/raw` and changes the default `/health` endpoint to add more
    interpretation, returning a 500 error code if there are no healthy
    workers locally.

--- app/server/tasks/http.py
+++ app/server/tasks/http.py
@@ -1,10 +1,12 @@
 """HTTP server to expose health check endpoint while worker is running."""
 
 import logging
+import socket
 from typing import Literal, TypedDict
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 from ..bg import BackgroundServer
 from .queue import queue
@@ -14,7 +16,77 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
-class WorkerHealth(TypedDict):
+class HealthCheckResponse(TypedDict):
+    """Response from Celery worker.
+
+    See:
+    https://docs.celeryq.dev/en/v5.3.6/reference/celery.app.control.html#celery.app.control.Control.ping
+    """
+
+    status: Literal["ok"] | Literal["error"]
+    error: str | None
+    total_workers: int
+    local_workers: int
+    host: str
+    healthy_workers: list[str]
+    unhealthy_workers: list[str]
+
+
+@app.get("/health")
+def health() -> JSONResponse:
+    """Health check endpoint.
+
+    Returns the health status of the workers.
+
+    There ought to be at least one healthy worker on the local machine
+    if this server is running. So if this does not hold, we return a 500 error.
+
+    Note that as long as there is one healthy worker locally, the server is
+    considered healthy, even if there are some unhealthy ones.
+
+    The full result can be inspected for remediation if necessary.
+
+    Returns:
+        JSONResponse: The `HealthCheckResponse` with a status of either 500 or 200.
+    """
+    health = queue.control.ping()
+    local = socket.gethostname()
+    address = f"@{local}"
+    healthy: list[str] = []
+    unhealthy: list[str] = []
+    for workers in health:
+        for worker, response in workers.items():
+            if not worker.endswith(address):
+                continue
+            if response.get("ok") == "pong":
+                healthy.append(worker)
+            else:
+                unhealthy.append(worker)
+
+    data = {
+        "total_workers": len(health),
+        "local_workers": len(unhealthy) + len(healthy),
+        "host": local,
+        "healthy_workers": healthy,
+        "unhealthy_workers": unhealthy,
+        "status": "ok",
+        "error": None,
+    }
+    status_code = 200
+
+    if not healthy:
+        logger.error("No healthy workers found.")
+        status_code = 500
+        data["error"] = "No healthy workers found"
+        data["status"] = "error"
+
+    return JSONResponse(
+        status_code=status_code,
+        content=data,
+    )
+
+
+class RawWorkerHealth(TypedDict):
     """Response from Celery worker.
 
     See:
@@ -24,15 +96,15 @@ class WorkerHealth(TypedDict):
     ok: Literal["pong"]
 
 
-class HealthCheckResponse(TypedDict):
+class WorkerRawHealthCheckResponse(TypedDict):
     """Health check response from the worker daemon."""
 
-    workers: list[dict[str, WorkerHealth]]
+    workers: list[dict[str, RawWorkerHealth]]
 
 
-@app.get("/health")
-def health() -> HealthCheckResponse:
-    """Health check endpoint."""
+@app.get("/health/raw")
+def health_raw() -> dict:
+    """Call `celery inspect ping` and return the result."""
     return {"workers": queue.control.ping()}
 
 
